bfbfab9ac88c5e855dfc5ef8570273ccc81ddf01
3774774728873737 <138348862+3774774728873737@users.noreply.github.com>
2023-07-03 21:12:58 +0500

    Update script.py

--- script.py
+++ script.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 
 @app.get("/")
-async def read_root(request: Request):
-    client_host = request.client.host
+async def read_root():
+    client_host = Request.client.host
     return {"client_host": client_host}
